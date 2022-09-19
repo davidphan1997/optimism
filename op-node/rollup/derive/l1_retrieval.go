d04394025cfe571b9d1e23e0ff219f322f52ff80
Joshua Gutow <jgutow@optimism.io>
2022-09-19 16:31:31 -0700

    op-node: Clean up pipeline
    Now that the engine queue is the only step stage, it is easy to
    consolidate different loops inside the derivation pipeline.

--- op-node/rollup/derive/l1_retrieval.go
+++ op-node/rollup/derive/l1_retrieval.go
@@ -21,7 +21,7 @@ type L1Retrieval struct {
 	datas DataIter
 }
 
-var _ PullStage = (*L1Retrieval)(nil)
+var _ ResetableStage = (*L1Retrieval)(nil)
 
 func NewL1Retrieval(log log.Logger, dataSrc CalldataProvider, prev *L1Traversal) *L1Retrieval {
 	return &L1Retrieval{
