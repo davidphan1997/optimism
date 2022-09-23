3ca51bda33278e4b1e49d30b206e59191cff739d
Joshua Gutow <jgutow@optimism.io>
2022-09-23 22:11:54 +0200

    op-node: Cleanup calldata source API
    This provides a new set of objects which provide the following API guarantees:
    - The opening of a data for a new L1 block will never.
    - If it failed to get transactions, it does so in the calls to `Next`.
    
    This greatly simplifies usage of this object when constructing new data. If
    the node did not keep track of the internal state, the external users of this
    API would have to keep track of this state in a more complex way.

--- op-node/rollup/derive/calldata_source.go
+++ op-node/rollup/derive/calldata_source.go
@@ -2,55 +2,104 @@ package derive
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"io"
 
 	"github.com/ethereum-optimism/optimism/op-node/eth"
 	"github.com/ethereum-optimism/optimism/op-node/rollup"
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/log"
 )
 
-// CalldataSource readers raw transactions from a given block & then filters for
-// batch submitter transactions.
-// This is not a stage in the pipeline, but a wrapper for another stage in the pipeline
-//
+type DataIter interface {
+	Next(ctx context.Context) (eth.Data, error)
+}
 
 type L1TransactionFetcher interface {
 	InfoAndTxsByHash(ctx context.Context, hash common.Hash) (eth.BlockInfo, types.Transactions, error)
 }
 
-type DataSlice []eth.Data
-
-func (ds *DataSlice) Next(ctx context.Context) (eth.Data, error) {
-	if len(*ds) == 0 {
-		return nil, io.EOF
-	}
-	out := (*ds)[0]
-	*ds = (*ds)[1:]
-	return out, nil
-}
-
-type CalldataSource struct {
+// CalldataSourceFactory readers raw transactions from a given block & then filters for
+// batch submitter transactions.
+// This is not a stage in the pipeline, but a wrapper for another stage in the pipeline
+type CalldataSourceFactory struct {
 	log     log.Logger
 	cfg     *rollup.Config
 	fetcher L1TransactionFetcher
 }
 
-func NewCalldataSource(log log.Logger, cfg *rollup.Config, fetcher L1TransactionFetcher) *CalldataSource {
-	return &CalldataSource{log: log, cfg: cfg, fetcher: fetcher}
+func NewCalldataSourceFactory(log log.Logger, cfg *rollup.Config, fetcher L1TransactionFetcher) *CalldataSourceFactory {
+	return &CalldataSourceFactory{log: log, cfg: cfg, fetcher: fetcher}
 }
 
-func (cs *CalldataSource) OpenData(ctx context.Context, id eth.BlockID) (DataIter, error) {
-	_, txs, err := cs.fetcher.InfoAndTxsByHash(ctx, id.Hash)
+// OpenData returns a CalldataSourceImpl. This struct implements the `Next` function.
+func (cs *CalldataSourceFactory) OpenData(ctx context.Context, id eth.BlockID) DataIter {
+	return NewCalldataSource(ctx, cs.log, cs.cfg, cs.fetcher, id)
+}
+
+// CalldataSource is a fault tolerant approach to fetching data.
+// The constructor will never fail & it will instead re-attempt the fetcher
+// at a later point.
+type CalldataSource struct {
+	// Internal state + data
+	open bool
+	data []eth.Data
+	// Required to re-attempt fetching
+	id      eth.BlockID
+	cfg     *rollup.Config // TODO: `DataFromEVMTransactions` should probably not take the full config
+	fetcher L1TransactionFetcher
+	log     log.Logger
+}
+
+// NewCalldataSource creates a new calldata source. It suppresses errors in fetching the L1 block if they occur.
+// If there is an error, it will attempt to fetch the result on the next call to `Next`.
+func NewCalldataSource(ctx context.Context, log log.Logger, cfg *rollup.Config, fetcher L1TransactionFetcher, block eth.BlockID) DataIter {
+	_, txs, err := fetcher.InfoAndTxsByHash(ctx, block.Hash)
 	if err != nil {
-		return nil, fmt.Errorf("failed to fetch transactions: %w", err)
+		return &CalldataSource{
+			open:    false,
+			id:      block,
+			cfg:     cfg,
+			fetcher: fetcher,
+			log:     log,
+		}
+	} else {
+		return &CalldataSource{
+			open: true,
+			data: DataFromEVMTransactions(cfg, txs, log.New("origin", block)),
+		}
 	}
-	data := DataFromEVMTransactions(cs.cfg, txs, cs.log.New("origin", id))
-	return (*DataSlice)(&data), nil
 }
 
+// Next returns the next piece of data if it has it. If the constructor failed, this
+// will attempt to reinitialize itself. If it cannot find the block it returns a ResetError
+// otherwise it returns a temporary error if fetching the block returns an error.
+func (cs *CalldataSource) Next(ctx context.Context) (eth.Data, error) {
+	if !cs.open {
+		if _, txs, err := cs.fetcher.InfoAndTxsByHash(ctx, cs.id.Hash); err == nil {
+			cs.open = true
+			cs.data = DataFromEVMTransactions(cs.cfg, txs, log.New("origin", cs.id))
+		} else if errors.Is(err, ethereum.NotFound) {
+			return nil, NewResetError(fmt.Errorf("failed to open calldata source: %w", err))
+		} else {
+			return nil, NewTemporaryError(fmt.Errorf("failed to open calldata source: %w", err))
+		}
+	}
+	if len(cs.data) == 0 {
+		return nil, io.EOF
+	} else {
+		data := cs.data[0]
+		cs.data = cs.data[1:]
+		return data, nil
+	}
+}
+
+// DataFromEVMTransactions filters all of the transactions and returns the calldata from transactions
+// that are sent to the batch inbox address from the batch sender address.
+// This will return an empty array if no valid transactions are found.
 func DataFromEVMTransactions(config *rollup.Config, txs types.Transactions, log log.Logger) []eth.Data {
 	var out []eth.Data
 	l1Signer := config.L1Signer()
